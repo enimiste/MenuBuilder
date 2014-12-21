5d2f9e7b78b60fd3dcf0d991356efa120e75adfa
Enimiste <nouni.elbachir@gmail.com>
2014-12-21 01:58:04 +0000

    Implements the class MenuItem

---
+++ src/Nouni/Menu/Impl/Menu/MenuItem.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nounielbachir
+ * Date: 21/12/14
+ * Time: 01:38
+ */
+
+namespace Nouni\Menu\Impl\Menu;
+
+use Nouni\Menu\Api\Menu\MenuItemInterface;
+
+/**
+ * Class MenuItem
+ * @package Nouni\Menu\Impl\Menu
+ * @author NOUNI EL BACHIR
+ */
+class MenuItem implements MenuItemInterface
+{
+    /**
+     * @var string
+     */
+    protected $titre;
+    /**
+     * @var string
+     */
+    protected $url;
+    /**
+     * @var string
+     */
+    protected $permission;
+    /**
+     * @var bool
+     */
+    protected $visibility;
+
+    function __construct($titre = '', $url = '', $permission = '', $visibility = true)
+    {
+        $this->permission = $permission;
+        $this->titre = $titre;
+        $this->url = $url;
+        $this->visibility = $visibility;
+    }
+
+
+    /**
+     * @return string
+     */
+    function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param string $titre
+     */
+    function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return string
+     */
+    function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    function get_permission()
+    {
+        return $this->permission;
+    }
+
+    /**
+     * @return bool
+     */
+    function is_visible()
+    {
+        return $this->visibility;
+    }
+
+    /**
+     * @param bool $value
+     */
+    function set_visibility($value)
+    {
+        $this->visibility = boolval($value);
+    }
+
+    /**
+     * @return array
+     */
+    function to_array()
+    {
+        if (!$this->is_visible())
+            return array();
+        return array(
+            'title' => $this->titre,
+            'url' => $this->url,
+            'permission' => $this->permission,
+        );
+    }
+}
